b464801dae859da4bdbd937847397ed5c870c621
kaan <kaandesu00@gmail.com>
2025-10-04 16:20:46 +0200

    chore: added sonner

--- app.vue
+++ app.vue
@@ -1,7 +1,11 @@
 <template>
   <NuxtLayout>
-    <NuxtLoadingIndicator height="5" />
+    <NuxtLoadingIndicator :height="5" />
     <NuxtPage />
     <Toaster />
   </NuxtLayout>
 </template>
+<script setup lang="ts">
+import { Toaster } from "@/components/ui/sonner";
+import "vue-sonner/style.css";
+</script>
